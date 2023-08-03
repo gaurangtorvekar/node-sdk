78f18002c9e5d94d71d0d9b89c3485fe06bc9cc7
Gaurang Torvekar <gaurangtorvekar@gmail.com>
2023-08-03 17:35:20 +0100

    Added a basic signer

---
+++ src/modules/bastion-signer/index.ts
@@ -0,0 +1,59 @@
+import { Deferrable } from "ethers/lib/utils";
+import { Provider, Web3Provider } from "@ethersproject/providers";
+import { Wallet, constants, utils, ethers } from "ethers";
+import { Signer } from "@ethersproject/abstract-signer";
+import { TransactionRequest } from "@ethersproject/abstract-provider";
+
+export interface BastionSignerOptions {
+	privateKey: string;
+	rpcUrl: string;
+	chainId: number;
+}
+
+export class BastionSigner extends Signer {
+	signer: Signer;
+	address: string;
+
+	constructor() {
+		super();
+	}
+
+	async init(externalProvider: Web3Provider, options?: BastionSignerOptions) {
+		try {
+			const address = await externalProvider.getSigner().getAddress();
+			this.signer = externalProvider.getSigner();
+		} catch (e) {
+			this.signer = new Wallet(options.privateKey, externalProvider);
+		}
+	}
+
+	async getAddress(): Promise<string> {
+		console.log("Inside getAddress = ", this.signer);
+		return this.signer.getAddress();
+	}
+
+	async signMessage(message: string | ethers.utils.Bytes): Promise<string> {
+		return this.signer.signMessage(message);
+	}
+
+	async getSigner(): Promise<Signer> {
+		return this.signer;
+	}
+
+	async getTransactionCount(blockTag?: string | number): Promise<number> {
+		return this.signer.getTransactionCount(blockTag);
+	}
+
+	async sendTransaction(transaction: any): Promise<any> {
+		return this.signer.sendTransaction(transaction);
+	}
+
+	async signTransaction(transaction: Deferrable<ethers.providers.TransactionRequest>): Promise<string> {
+		throw new Error("signTransaction Method not implemented.");
+	}
+
+	connect(provider: Provider): ethers.Signer {
+		throw new Error("Method not implemented.");
+	}
+}
+
