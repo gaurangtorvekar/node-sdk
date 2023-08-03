f3528b9501a6646345a932aecdfab80607122373
Gaurang Torvekar <gaurangtorvekar@gmail.com>
2023-08-03 21:24:14 +0100

    Added bastion signer to the base class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import { SmartWallet } from './modules/smart-wallet';
+import { SmartWallet } from "./modules/smart-wallet";
+import { BastionSigner } from "./modules/bastion-signer";
 
 export class Bastion {
-  smartWallet: SmartWallet;
+	smartWallet: SmartWallet;
+	bastionSigner: BastionSigner;
 
-  constructor(config: { apiKey: string; baseUrl?: string }) {
-    this.smartWallet = new SmartWallet(config);
-  }
+	constructor(config: { apiKey: string; baseUrl?: string }) {
+		this.smartWallet = new SmartWallet(config);
+		this.bastionSigner = new BastionSigner();
+	}
 }
+
